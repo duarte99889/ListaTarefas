6d26fc380aebee7207149d53277a84ad4d9e3be2
duarte99889 <duarte00@icloud.com>
2024-10-30 12:23:28 +0000

    meia estrutura

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Pirmeiro teste</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>To-do List</h1>
       </header>
+      <div>
+        <p>Barras Pesquisa</p>
+        <button>Adicionar Tarefa</button>
+        <p>Secção com todos</p>
+        <p>secção Ativa</p>
+        <p>Secção dos completos</p>
+      </div>
+      <div>
+        Lista com as tarefas em si
+      </div>
     </div>
   );
 }
